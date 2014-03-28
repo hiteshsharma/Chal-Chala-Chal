3fc73b0e7d78d76eced841e87df4d633ed90ed34
Hitesh Sharma <hitesh@slideshare.com>
2014-03-28 18:52:43 +0530

    committing repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TravelFriend::Application.config.secret_key_base = '8742902033be623354e4d8e35eee62f72d571a85d8ab446b6baeb36f12ce794c7ead866b456acc220a26963d0c6233dd3a4ad2b1cf7cfc91ab73087a1553a611'
